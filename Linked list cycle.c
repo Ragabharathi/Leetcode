7ca4d4b03e1287f4cea2950d7a96856899b093c3
Ragabharathi <84565134+Ragabharathi@users.noreply.github.com>
2021-07-03 09:58:01 +0530

    Create Linked list cycle.c

---
+++ Linked list cycle.c
@@ -0,0 +1,17 @@
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+
+
+
+bool hasCycle(struct ListNode *head) {
+    if(head==NULL){return false;}
+    struct ListNode *fast=head;
+    struct ListNode *slow=head;
+    while(fast!=NULL&&fast->next!=NULL){
+        fast=fast->next->next;
+        slow=slow->next;
+        if(slow==fast){return true;}
+    }
+    return false;
+}
