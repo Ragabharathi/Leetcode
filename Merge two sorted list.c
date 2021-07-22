ccd559ad4a9887711bff8689361af5edfbc187ef
Ragabharathi <84565134+Ragabharathi@users.noreply.github.com>
2021-07-22 21:27:53 +0530

    Create Merge two sorted list.c

---
+++ Merge two sorted list.c
@@ -0,0 +1,32 @@
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+struct ListNode* mergeTwoLists(struct ListNode* l1, struct ListNode* l2)
+{
+    struct ListNode head;
+    struct ListNode *h = &head;
+    
+    if (l1 == NULL && l2 == NULL)
+        return NULL;
+    
+    while (l1 && l2) {
+        if (l1->val < l2->val) {
+            h->next = l1;
+            l1 = l1->next;
+            h = h->next;
+        } else {
+            h->next = l2;
+            l2 = l2->next;
+            h = h->next;
+        }
+    }
+
+    if (l1) {
+        h->next = l1;
+    }
+    if (l2) {
+        h->next = l2;
+    }
+  
+    return head.next;
+}
