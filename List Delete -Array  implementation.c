59a5c950039fe50ed206a3f79b6b837421efe55d
Ragabharathi <84565134+Ragabharathi@users.noreply.github.com>
2021-07-23 07:45:09 +0530

    Update List Delete -Array  implementation.c

--- List Delete -Array  implementation.c
+++ List Delete -Array  implementation.c
@@ -2,42 +2,64 @@
 #include<stdlib.h>
 #define MAX 1000
 int arr[MAX],size=-1;
-void deleteAtLast(){
+int deleteAtLast(){
+     if(size==-1)
+    {
+        printf("Array Empty");
+        return 0 ;
+    }
     size--;
+    return 1;
 }
-void deleteAtBegining(int data){
+int deleteAtBegining(int data){
+     if(size==-1)
+    {
+        printf("Array Empty");
+        return 0 ;
+    }
     
     for(int i=0;i<size-1;i++)
     {
         arr[i]=arr[i+1];
     }
     size--;
+    return 1;
         
     
 }
-void deleteAtMiddle(int p){
-  
+int deleteAtMiddle(int p){
+     if(size==-1)
+    {
+        printf("Array Empty");
+        return 0 ;
+    }
+    if(p>size)
+    {
+        printf("Array size less than position\n");
+        return 1;
+    }
     for(int i=p;i<size-1;i++)
     {
         arr[i]=arr[i-1];
     }
     size--;
-    
+    return 1;
     
 }
 void append(int data){
     arr[++size]=data;
 }
-void display(){
+int display(){
     if(size==-1)
     {
         printf("Array Empty");
-        return ;
+        return 0;
     }
     printf("Elements in the array:");
-    for(int i=0;i<size;i++){
+    for(int i=0;i<=size;i++){
         printf("%d ",arr[i]);
     }
+    return 1;
 }
 int main()
 {
@@ -49,22 +71,31 @@ int main()
         scanf("%d",&data);
         append(data);
     }
-        printf("\nEnter the choice to delete:\n1.deleteAtLast\n2.deleteAtBegining\n3.deleteAtMiddle\n");
+    while(1){
+        printf("\nEnter the choice to delete:\n1.deleteAtLast\n2.deleteAtBegining\n3.deleteAtMiddle\n4.exit\n");
         scanf("%d",&ch);
         switch(ch){
             case 1:
-            deleteAtLast(data);
+            if(!deleteAtLast(data))
+            return 0;
             break;
             case 2:
-            deleteAtBegining(data);
+            if(!deleteAtBegining(data))
+            return 0;
             break;
             case 3:
             printf("Enter the position to insert:\n");
             scanf("%d",&p);
-            deleteAtMiddle(p);
+            if(!deleteAtMiddle(p))
+            return 0;
             break;
+            case 4:
+            exit(0);
         }
+       if(! display())
+       return 0;
+    }
         
-    display();
+    
     return 0;
 }
